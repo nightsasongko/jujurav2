d9aed1e75aa836f9a57908bbd93ae1ccd1b90578
nightsasongko <nightsasongko.website@gmail.com>
2022-03-08 11:08:17 +0700

    jujura

---
+++ resources/views/product.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
+</head>
+<body>
+    <h2>Product</h2>
+    <table>
+        <tr>
+            <td>product_id</td>
+            <td>:</td>
+            <td>data</td>
+        </tr>
+        <tr>
+            <td>name</td>
+            <td>:</td>
+            <td>data</td>
+        </tr>
+        <tr>
+            <td>price</td>
+            <td>:</td>
+            <td>data</td>
+        </tr>
+        <tr>
+            <td>stock</td>
+            <td>:</td>
+            <td>data</td>
+        </tr>
+    </table>
+</body>
+</html>
